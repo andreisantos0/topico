64498eaa939e402bf67219d40271e1317e2a191c
Andrei <Andreisantos153@gmail.com>
2018-08-27 20:01:28 -0300

    Primeiro Programa Spring

---
+++ spring/fvs/Pessoa.java
@@ -0,0 +1,29 @@
+package fvs;
+
+public class Pessoa {
+
+	private Interger id;
+	private String nome;
+	private String cpf;
+	public Interger getId() {
+		return id;
+	}
+	public void setId(Interger id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	}
+
+
